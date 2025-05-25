4c5b698b9116d5359d094437cd25249c78748500
asemaikauas <108888800+asemaikauas@users.noreply.github.com>
2025-05-26 03:42:20 +0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,27 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
 
-
+# Initialize extensions
 db = SQLAlchemy()
 migrate = Migrate()
-
 
 def create_app():
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = 'resistwise'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:ad7Vgy26@localhost/resistapp'
- 
+    db_url = os.environ.get('DATABASE_URL')
+    
+    if db_url and db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://", 1)
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url or 'postgresql://postgres:ad7Vgy26@localhost/resistapp'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'resistwise')
+
     db.init_app(app)
-    migrate.init_app(app, db) 
+    migrate.init_app(app, db)
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
@@ -27,11 +33,17 @@ def create_app():
     def load_user(user_id):
         return User.query.get(int(user_id))
 
-
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    with app.app_context():
+        db.create_all()
+
     return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
